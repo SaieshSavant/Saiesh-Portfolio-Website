4ddaf399b512adcff8597df003fcb106b2307a1e
Saiesh Savant <saieshsawant70@gmail.com>
2024-08-24 18:52:41 +0530

    Bug Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 // pages/_app.js
 import '../styles/globals.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
